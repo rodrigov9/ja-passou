c9d2d4da60477a1df3a89d035ab7d5085ba3419a
Rodrigo Vitorino <49363242+rodev9@users.noreply.github.com>
2026-02-19 02:33:28 +0000

    chore: change version for the first release

--- app.config.ts
+++ app.config.ts
@@ -16,7 +16,7 @@ export default {
   name: appName,
   slug: 'japassou',
   scheme: appScheme,
-  version: '1.0.0',
+  version: '0.1.0',
   orientation: 'portrait',
   icon: `./assets/${iconsFolder}icon.png`,
   userInterfaceStyle: 'automatic',
